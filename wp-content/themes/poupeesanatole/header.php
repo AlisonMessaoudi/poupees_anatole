c1ae3fb3b4fc63f7ccba26f0d4a6dfe4cd7c9741
Alison Messaoudi <alisonmessaoudi@macbook.home>
2021-02-12 13:09:50 +0100

    ajout des images et typographie

--- wp-content/themes/poupeesanatole/header.php
+++ wp-content/themes/poupeesanatole/header.php
@@ -13,9 +13,6 @@
         <!-- FAVICON -->
         <link rel="icon" type="image/png" href="#"/>
 
-        <!-- FONTAWESOME -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-
         <!-- ANIMATE CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
         
