d6322920ed56fcf26e991709b4791f72ee26e5c4
Alison Messaoudi <alisonmessaoudi@macbook.home>
2021-03-05 15:42:18 +0100

    optimisation code sass et responsive page project

--- wp-content/themes/poupeesanatole/template-parts/project.php
+++ wp-content/themes/poupeesanatole/template-parts/project.php
@@ -36,7 +36,8 @@ get_header();
 
     <!-- SECTION 2 : LES POUPEES -->
     <section class="container-fluid project__presentation">
-
+        
+        <!-- BLOC FORMES -->
         <div class="project__presentation__formes">
 
             <!-- Forme Left -->
@@ -56,15 +57,17 @@ get_header();
             <!-- ILLUSTRATION 1 -->
             <div class="row project__presentation__illustration">
 
+                <!-- BLOC IMAGE -->
                 <div class="col-lg-6 illustration__img__left">
                     
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/07_images/poupees_presentation02.png" alt="Une représentation des diversités - Thomas Huard les Poupées Anatoles"/>
                 
                 </div>
                 
+                <!-- BLOC TEXTE -->
                 <div class="col-lg-6 illustration__txt__left">
                     
-                    <h3>Les Poupées</h3>
+                    <h2>Les Poupées</h2>
 
                     <p><span>Ces poupées sont inclusives.</span> Les différentes morphologies, âges, et origines sont représentées par chacune d'entre elles. Également, nous avons voulu représenter des éléments trop peu visibles sur les représentations habituelles des femmes et des hommes. Ici, nous pouvons parler de poils, de hanches épaisses et de seins tombants, de vulves et de pénis...</p>
                 
@@ -76,12 +79,14 @@ get_header();
             <!-- ILLUSTRATION 2 -->
             <div class="row project__presentation__illustration">
 
+                <!-- BLOC TEXTE -->
                 <div class="col-lg-6 illustration__txt__right">
 
                     <p>La particularité de ces poupées repose également sur la possibilité qu'elles ont de <span>modifier leurs émotions.</span> Grâce à un système de modification de leur expression faciale, il est alors possible de parler de choses incontournables lorsqu'il s'agit de sexualité: les sentiments, le consentement, le plaisir etc...</p>
                 
                 </div>
 
+                <!-- BLOC IMAGE -->
                 <div class="col-lg-6 illustration__img__right">
                 
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/07_images/poupees_presentation.png" alt="Poupées émotives - Thomas Huard les Poupées Anatoles"/>
@@ -100,10 +105,10 @@ get_header();
         <!-- TITRE : LES COLLABORATEURS -->
         <div class="row collaborateurs__titre">
                 
-            <h2>Les Collaborateurs</h2>
+            <h2>Collaborateurs</h2>
             
             <button>
-                <a href="<?php echo home_url('/collaborateurs'); ?>">Tous les collaborateurs&nbsp;&nbsp;<i class="fa fa-chevron-right"></i></a>
+                <a href="<?php echo home_url('/collaborateurs'); ?>">Les découvrir&nbsp;&nbsp;<i class="fa fa-chevron-right"></i></a>
             </button>
         
         </div>
@@ -229,10 +234,10 @@ get_header();
     <section class="container p-0 documents">
 
         <div>
-            <h5>Les documents pour aller plus loin</h5>
+            <h2>Les documents pour aller plus loin</h2>
         </div>
 
-        <div class="row m-0 liste__documents">
+        <div class="slider__wrapper liste__documents">
 
             <!-- DOCUMENT 1 -->
             <div class="document">
